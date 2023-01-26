d4aef29b8c3b49fd1633bfa308df134eab5ef197
slayernominee <slayernominee_git@proton.me>
2023-01-26 22:39:18 +0100

    some todos in the code added

--- controler/accounts.py
+++ controler/accounts.py
@@ -2,6 +2,9 @@ import sqlite3
 
 db = 'data/accounts.db'
 
+# TODO: load all values in the response and load the matching permissions 
+# TODO: create a new user with zero permissions 
+
 def check(username: str, password_hash: str) -> dict:
     with sqlite3.connect(db) as conn:
         c = conn.cursor()
