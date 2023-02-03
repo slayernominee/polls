78f31a6d131ebacaa9dd491a5c6f7003ec95ed53
slayernominee <slayernominee_git@proton.me>
2023-02-03 15:31:40 +0100

    question progress added by qbq

--- templates/js/qbq.js
+++ templates/js/qbq.js
@@ -17,6 +17,13 @@ document.getElementById('next_page_button').addEventListener('click', function()
     }
     document.getElementById('page_' + current_page_index).style.display = 'block';
     document.getElementById('previous_page_button').style.display = 'block';
+
+    current_page_number_element = document.getElementById('current_page_number');
+    
+    if (current_page_number_element !== null) {
+        current_page_number_element.innerHTML = current_page_index + 1;
+
+    }
 });
 
 document.getElementById('previous_page_button').addEventListener('click', function() {
@@ -29,8 +36,21 @@ document.getElementById('previous_page_button').addEventListener('click', functi
     document.getElementById('next_page_button').style.display = 'block';
     document.getElementById('submit_button').style.display = 'none'
 
+    current_page_number_element = document.getElementById('current_page_number');
+    
+    if (current_page_number_element !== null) {
+        current_page_number_element.innerHTML = current_page_index + 1;
+
+    }
+
 });
 
 document.getElementById('submit_button').addEventListener('click', function() {
     document.getElementById('poll_form').submit();
 });
+
+max_page_number_element = document.getElementById('max_page_number');
+if (max_page_number_element !== null) {
+    max_page_number_element.innerHTML = page_max_index + 1;
+
+}
